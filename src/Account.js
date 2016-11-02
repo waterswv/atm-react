29bf525e666dbc251237306991ce4d99fd03d6e4
Riley Dallas <rldaggie@gmail.com>
2016-11-02 09:16:24 -0500

    Set balance to be on state

--- src/Account.js
+++ src/Account.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      balance: 0
+    }
+  }
+
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">$0</div>
+        <div className="balance">${this.state.balance}</div>
         <input type="text" placeholder="enter an amount" />
         <input type="button" value="Deposit" />
         <input type="button" value="Withdraw" />
